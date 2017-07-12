580b142f2abd17250860dc94f00a0113f44f9674
Vincent Hounshell <hounvs@gmail.com>
2017-07-11 21:36:32 -0400

    Remove old table show/hide

---
+++ TryHarder/Scripts/Site.js
@@ -0,0 +1,18 @@
+﻿/*
+ * Bind page address to modal. Source:
+ * https://stackoverflow.com/questions/34154370/bootstrap-3-x-how-to-have-url-change-upon-clicking-modal-trigger
+ */
+
+$(window.location.hash).modal('show');
+$('a[data-toggle="modal"]').click(function () {
+    window.location.hash = $(this).attr('href');
+});
+
+function revertToOriginalURL() {
+    var original = window.location.href.substr(0, window.location.href.indexOf('#'))
+    history.replaceState({}, document.title, original);
+}
+
+$('.modal').on('hidden.bs.modal', function () {
+    revertToOriginalURL();
+});
